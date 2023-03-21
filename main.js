cc2ca153f4abd4be885076cbccf5871a8d70e66a
Ovchinnikov Danila <52536804+Danik-Off@users.noreply.github.com>
2023-03-21 09:40:55 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,46 @@
-const openRequest = indexedDB.open("todoDB",1);
+const openRequest = indexedDB.open("todoDB", 1);
 let db;
-openRequest.onupgradeneeded =()=>{
-db = openRequest.result;
-if (!db.objectStoreNames.contains('books')) { // если хранилище "books" не существует
-    db.createObjectStore('books', {keyPath: 'id'}); // создаём хранилище
+openRequest.onupgradeneeded = () => {
+  db = openRequest.result;
+  if (!db.objectStoreNames.contains("books")) {
+    // если хранилище "books" не существует
+    db.createObjectStore("books", { keyPath: "id" }); // создаём хранилище
   }
-
-}
-
-openRequest.onsuccess =()=>{
-db = openRequest.result;
-}
-
-window.onload =()=>{
-  
-  
 };
-   
+
+openRequest.onsuccess = () => {
+  db = openRequest.result;
+};
+
+window.onload = () => {
+  const inputNewToDo = document.querySelector(".new-todo");
+  inputNewToDo.addEventListener("blur", addNewTask);
+  openRequest.onsuccess = () => {};
+};
+
+function addNewTask(e) {
+  const text = e.value;
+  const id = 0;
+  const sampleTask = `<li data-id="${id}">
+	                    <div class="view">
+		                  <input class="toggle" type="checkbox">
+		                  <label>${text}</label>
+		                  <button class="destroy"></button>
+	                    </div>
+                      </li>`;
+  const tempVar = document.createElement("div");
+  tempVar.innerHTML = sampleTask;
+  const domElementTask = tempVar.firstChild;
+  //domElementTask.addEventListener();
+  
+}
+
+function hadlerTask(e)
+{
+  const task = e.currentTarget;
+  alert(this);
+}
+
+function editTask() {}
+
+function deleteTask() {}
